72f4e9b45fb085c9198234538c65dc9522a343ed
mor <whiteluna@gmail.com>
2025-10-24 23:46:15 +0300

    Add Python 3.14t to CI

--- setup.py
+++ setup.py
@@ -92,6 +92,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "License :: OSI Approved :: Apache Software License",
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
